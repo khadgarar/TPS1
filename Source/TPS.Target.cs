a9bc4e863dda9427cf1a3ca323ed96ec4bc47b78
khadgarar <ronec2002@gmail.com>
2023-03-16 14:37:41 +0300

    try 1

---
+++ Source/TPS.Target.cs
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+using System;
+
+public class TPSTarget : TargetRules
+{
+    public TPSTarget(TargetInfo Target) : base(Target)
+    {
+        Console.WriteLine("TPS game target --------->");
+        Type = TargetType.Game;
+        DefaultBuildSettings = BuildSettingsVersion.V2;
+        ExtraModuleNames.Add("TPS");
+    }
+}
